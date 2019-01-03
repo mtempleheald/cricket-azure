c7b70f45e974dc4545fad5724a42080fcc4601ef
mtempleheald@hotmail.com <mtempleheald@hotmail.com>
2019-01-03 20:13:22 +0000

    switch to az cli over azurerm for configuring static hosting

---
+++ deployment/Enable-StaticWebsite.ps1
@@ -0,0 +1,7 @@
+﻿Param
+(
+    [string]$resourceGroup = "mthcricket01",
+    [string]$storageAccountName = "mthcricket01uistorage"
+)
+az extension add --name storage-preview
+az storage blob service-properties update --account-name "$storageAccountName" --static-website --404-document 404.html --index-document index.html
