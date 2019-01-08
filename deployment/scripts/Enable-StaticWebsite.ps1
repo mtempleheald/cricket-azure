bb351a8fe260a1ad8230e3e02f5c775930a048b5
mtempleheald@hotmail.com <mtempleheald@hotmail.com>
2019-01-08 11:25:16 +0000

    remove unused script

--- deployment/scripts/Enable-StaticWebsite.ps1
+++
@@ -1,9 +0,0 @@
-﻿Param
-(
-    [string]$resourceGroup = "mthcricket01",
-    [string]$storageAccountName = "mthcricket01uistorage"
-)
-
-$storageAccountKey = Get-AzStorageAccountKey -ResourceGroupName $resourceGroup -Name $storageAccountName | Where-Object { $_.KeyName -eq 'key1' } | Select-Object -ExpandProperty Value
-$storageAccountContext = New-AzStorageContext -StorageAccountName "$storageAccountName" -StorageAccountKey "$storageAccountKey"
-Enable-AzStorageStaticWebsite -IndexDocument index.html -ErrorDocument404Path 404.html -Context $storageAccountContext
